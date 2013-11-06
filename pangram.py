127ffce1dd9d6c190492a551d0c8c33aab60ba82
steven <stevenwiggins83@gmail.com>
2013-11-06 16:06:26 -0500

    Slight code cleanup and removal of broken script

--- pangram.py
+++ pangram.py
@@ -1,23 +1,16 @@
 import sys
 import string
 
-def main():
-    for arg in sys.argv[1:]:
-        pangram(arg)
-
-
 def pangram(pathname):
 
     letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    
-    f = open(pathname,'r')
 
-    for line in f.readlines():
+    for line in open(pathname, 'r').readlines():
         missing = []
         for letter in letters:
             if letter not in line.lower():
                 missing.append(letter)
-        if len(missing)>0:
+        if len(missing) > 0:
             for miss in missing:
                 sys.stdout.write(miss)
         else:
@@ -25,4 +18,5 @@ def pangram(pathname):
         print
 
 if __name__ == "__main__":
-    main()
+    for arg in sys.argv[1:]:
+        pangram(arg)
