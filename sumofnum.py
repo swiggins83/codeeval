6f6351e4b99b575f16a8837e49743c8f31f2db7d
steven <stevenwiggins83@gmail.com>
2013-11-06 16:21:11 -0500

    Slight code cleanup and removal of broken scripts? // Should refactor sumofnum

--- sumofnum.py
+++ sumofnum.py
@@ -1,14 +1,12 @@
 import sys
 
-def main():
-    for arg in sys.argv[1:]:
-        f = open(arg,'r')
-        
-        for line in f.readlines():
-            summ = 0
-            for i in range(0,len(line)-1):
-                summ += int(line[i])
-            print summ
+def main(pathname):
+	for line in open(pathname, 'r').readlines():
+		summ = 0
+		for i in range(0,len(line)-1):
+			summ += int(line[i])
+		print summ
 
 if __name__ == "__main__":
-    main()
+    for arg in sys.argv[1:]:
+		main(arg)
