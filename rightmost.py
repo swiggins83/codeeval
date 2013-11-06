429f3e5e974b156903fc540e2021d9cb8f5b9e17
steven <stevenwiggins83@gmail.com>
2013-11-06 16:17:44 -0500

    Slight code cleanup

--- rightmost.py
+++ rightmost.py
@@ -1,25 +1,15 @@
 import sys
 import string
 
-
-def main():
-
-    for arg in sys.argv[1:]:
-        rightmost(arg)
-
-
 def rightmost(pathname):
     """Print position of character in a string."""
 
-    f = open(str(pathname), 'r')
-
-    for s in f.readlines():
+    for s in open(pathname, 'r').readlines():
         if len(s) > 0:
             res = s.split(",")
 
-            result = res[0].rfind(res[1].split("\n")[0])
-
-            print result
+            print res[0].rfind(res[1].split("\n")[0])
 
 if __name__ == '__main__':
-    main()
+    for arg in sys.argv[1:]:
+        rightmost(arg)
