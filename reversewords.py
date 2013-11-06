2b99a68b5719c7775e831c43df1b74ff3a604c5a
steven <stevenwiggins83@gmail.com>
2013-11-06 16:15:55 -0500

    Refactored reverse words code and add .gitignore

--- reversewords.py
+++ reversewords.py
@@ -1,25 +1,13 @@
 import sys
 
-def main():
-    for arg in sys.argv[1:]:
-        reversewords(arg)
-
-
 def reversewords(pathname):
     """Reverse two words separated by a whitespace."""
 
-    f = open(pathname,'r')
-
-    for line in f.readlines():
-        if len(line) > 0:                
-            split = str(line).split(" ")
-            for i in range(0,len(split)):
-                if "\n" in split[len(split)-1-i]:
-                    sys.stdout.write(split[len(split)-1-i].split("\n")[0] + ' ')
-                else:
-                    sys.stdout.write(split[len(split)-1-i] + ' ')
-
-            print
+    for line in open(pathname, 'r').readlines():
+        if len(line) > 0:
+            split = line.split("\n")[0].split(" ")
+            print split[1] + " " + split[0]
 
 if __name__ == "__main__":
-    main()
+    for arg in sys.argv[1:]:
+        reversewords(arg)
